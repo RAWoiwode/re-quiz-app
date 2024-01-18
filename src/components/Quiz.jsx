a2ca6db535872a7578014c6ea5658bf5b6944ee2
Ralph Woiwode <rawoiwode@gmail.com>
2024-01-18 08:18:47 -0600

    Adding basic shuffling logic for questions; Catching end state when there are no more questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
-
+import quizComplete from "../assets/quiz-complete.png";
 import QUESTIONS from "../questions";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeIndex = userAnswers.length;
+  const isQuizComplete = activeIndex === QUESTIONS.length;
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
   };
+
+  if (isQuizComplete) {
+    return (
+      <div id="summary">
+        <img src={quizComplete} alt="Quiz completed" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
